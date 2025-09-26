e128bf580ce8507cd9cb7beb436a0142ba846b2d
GuilherrSilva <abacaxiespinhudo@gmail.com>
2025-09-25 21:53:51 -0300

    dia de hoje
    dia

--- exercicios-aluno/Arrays/script/main.js
+++ exercicios-aluno/Arrays/script/main.js
@@ -1,50 +1,45 @@
-const { version } = require("react");
+/**
+Exibir no console as horas de uso para cada dia da semana de maneira formatada,
+adicionando um sinal verde, amarelo ou vermelho, de acordo com um critério seu de horas
+adequadas de uso da rede, por exemplo:
 
-let usoDiasDaSemana = [2, 2, 2.4, 5, 1.2, 0.5, 12] // Domingo
+- 0-1 hora por dia = verde
+- 1-3 horas por dia = amarelo
+- 3 horas ou mais por dia = vermelho
+ */
+let usoDiasDaSemana = [2, 2.4, 5, 1.2, 0.5];
 let diasDaSemana = [
-'Domingo',
-'Segunda-feira',
-'Terça-feira',
-'Quarta-feira',
-'Quinta-feira',
-'Sexta-feira',
-'Sabado'
-]
+	'Segunda-feira',
+	'Terça-feira  ',
+	'Quarta-feira ',
+	'Quinta-feira ',
+	'Sexta-feira  ',
+];
 
 // Determinar se o uso do dia é verde, amarelo ou vermelho
 const indicadorDeUso = (numeroDeHoras) => {
-    if (numeroDeHoras <= 1) {
-        return '🟢';
-    } else if (numeroDeHoras <= 3) {
-        return '🟡';
-    } else {
-        return '🔴';
-    }
+	if ( numeroDeHoras <= 1 ) {
+		return '🟢';
+	} else if ( numeroDeHoras <= 3 ) {
+		return '🟡';
+	} else {
+		return '🔴';
+	}
 }
 
-//console.log( 'Domingo:' + indicadorDeUso(usoDiasDaSemana[0]));
-//console.log( 'Segunda-feira:' + indicadorDeUso(usoDiasDaSemana[1]));
-//console.log('Terça-feira:' + indicadorDeUso(usoDiasDaSemana[2]));
-//console.log('Quarta-feira:' + indicadorDeUso(usoDiasDaSemana[3]));
-//console.log('Quinta-feira:' + indicadorDeUso(usoDiasDaSemana[4]));
-//console.log('Sexta-feira:' + indicadorDeUso(usoDiasDaSemana[5]));
-//console.log('Sabado:' + indicadorDeUso(usoDiasDaSemana[6]));
+for(let contador = 0; contador < usoDiasDaSemana.length ; contador++) {
+	let farol = indicadorDeUso(usoDiasDaSemana[contador]);
+	let dia = diasDaSemana[contador];
+	let horas = usoDiasDaSemana[contador];
+	// let mensagem =  'hora de uso';
 
-for(let contador = 0; contador <= 6; contador++) {
-    let farol = indicadorDeUso(usoDiasDaSemana[contador]);
-    let dia = diasDaSemana[contador];
-    let horas = usoDiasDaSemana[contador];
+	// if ( horas > 1 ) { 
+	// 	mensagem = 'horas de uso';
+	// }
 
-    if (horas > 1) {
-        let mensagem = 'horas de uso'
-    } else {
-        let mensagem = 'horas de uso'
-    }
+	let mensagem = (horas > 1) ? 'horas de uso' : 'hora de uso'; // Operador ternário
 
-    console.log(`${farol} 📆 ${ dia } | ${ horas} horas de uso`);
+	console.log(`${farol} 📅 ${dia} | ${horas.toFixed(2)} ${mensagem}`);
 }
 
-//console.log(typeof usoDiasDaSemana);
-//console.log(typeof usoDiasDaSemana[0]);
-//console.log(typeof indicadorDeUso);
 
---
+++ exercicios-aluno/DOM/js/main.js
@@ -0,0 +1,7 @@
+let cortico = document.getElementById('o-cortico');
+
+console.log(cortico)
+
+let a = document.getElementsByTagName('a')
+
+console.log(a)
